b77e88420fba012737686a0c4ec6390ae2922de6
yokesh3339 <37687524+yokesh3339@users.noreply.github.com>
2018-03-23 13:48:13 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+int main()
+{
+ int n;
+ printf("enter the number:");
+ scanf("%d",&n);
+ 
+       if(n>0)
+       {
+       	printf("\n the number is positive");
+       }
+       else if(n==0)
+       {
+       	printf("\n the number is zero");
+       	
+       }
+      else
+      {
+       printf("the number is negative");
+      }
+  
+  return 0;
+}
+       
+ 
+ 
